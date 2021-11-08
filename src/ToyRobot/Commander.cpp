93838b31263e495b3d72595e536fd50fadde853d
Kushan Fernando <kushan.fernando@gallagher.com>
2021-11-08 15:48:26 +1300

    a couple of issue fixes

---
+++ src/ToyRobot/Commander.cpp
@@ -0,0 +1,195 @@
+/*
+ *	The MIT License (MIT)
+ *	Copyright © 2021 Kushan Fernando (info@kushan.me)
+ *
+ *	Permission is hereby granted, free of charge, to any person obtaining
+ *  a copy of this software and associated documentation files (the “Software”),
+ *  to deal in the Software without restriction, including without limitation
+ *  the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ *  sell copies of the Software, and to permit persons to whom the Software is furnished
+ *  to do so, subject to the following conditions:
+ *
+ *	The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ *	THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ *  INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ *  PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ *  FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ *  ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include "Commander.h"
+#include <algorithm>
+
+CommanderBase::CommanderBase(ToyRobot& robot, LoggerBase& logger)
+    : m_robot(robot),
+    m_logger(logger)
+{
+    for (const auto& entry : m_facingDirections)
+        m_facingDirectionStrings.emplace(entry.second, entry.first);
+}
+
+void CommanderBase::Launch()
+{
+    m_logger.Info("Toy robot starting..");
+
+    auto cmd = cmdUNKNOWN;
+    while (cmd != cmdEXIT)
+    {
+        std::vector<std::string> args;
+
+        cmd = GetCommand(args);
+        switch (cmd)
+        {
+        case cmdPLACE:
+            Place(args);
+            break;
+        case cmdMOVE:
+            Move();
+            break;
+        case cmdTURN_LEFT:
+        {
+            const auto sucess = m_robot.TryTurnLeft();
+            if (!sucess)
+                m_logger.Error("Turn left failed.");
+            break;
+        }
+        case cmdTURN_RIGHT:
+        {
+            const auto sucess = m_robot.TryTurnRight();
+            if (!sucess)
+                m_logger.Error("Turn right failed.");
+            break;
+        }
+        case cmdREPORT:
+            Report();
+            break;
+        case cmdUNKNOWN:
+        default:
+            m_logger.Error("Unknown command");
+            break;
+        }
+    }
+
+    m_logger.Info("Toy robot quitting..");
+}
+
+void CommanderBase::Place(std::vector<std::string>& xargs)
+{
+    if (xargs.size() != 1)
+    {
+        m_logger.Error("Invalid number of arguments for place command. Command expects 3 arguments in the form of (place x,y,direction)");
+        return;
+    }
+
+    const auto args = Split(xargs[0], ",");
+
+    if (args.size() != 3)
+    {
+        m_logger.Error("Invalid number of arguments for place command. Command expects 3 arguments in the form of (place  x,y,direction)");
+        return;
+    }
+
+    int x = 0;
+    if (!TryParseInt(args[0], x))
+    {
+        m_logger.Error("Invalid x value: " + args[0]);
+        return;
+    }
+
+    int y = 0;
+    if (!TryParseInt(args[1], y))
+    {
+        m_logger.Error("Invalid y value : " + args[1]);
+        return;
+    }
+
+    const auto facingDirectionItr = m_facingDirections.find(args[2]);
+    if (facingDirectionItr == m_facingDirections.end())
+    {
+        m_logger.Error("Invalid facing direction: " + args[2]);
+        return;
+    }
+
+    const auto sucess = m_robot.TryPlace(x, y, facingDirectionItr->second);
+    if (!sucess)
+    {
+        m_logger.Error("Placement failed.");
+        return;
+    }
+}
+
+void CommanderBase::Move()
+{
+    m_robot.TryMove();
+}
+
+void CommanderBase::Report()
+{
+    uint8_t x;
+    uint8_t y;
+    FacingDirection facingDirection;
+
+    m_robot.Report(x, y, facingDirection);
+    m_logger.Info("Output: " + std::to_string(x) + "," + std::to_string(y) + "," + m_facingDirectionStrings[facingDirection]);
+}
+
+std::vector<std::string> CommanderBase::Split(const std::string& str, const std::string& delimiter)
+{
+    std::vector<std::string> tokens;
+    size_t prev = 0;
+    size_t pos = 0;
+    do
+    {
+        pos = str.find(delimiter, prev);
+        if (pos == std::string::npos)
+            pos = str.length();
+
+        std::string token = str.substr(prev, pos - prev);
+        if (!token.empty())
+            tokens.push_back(token);
+
+        prev = pos + delimiter.length();
+
+    } while (pos < str.length() && prev < str.length());
+
+    return tokens;
+}
+
+std::string CommanderBase::ToLower(std::string str)
+{
+    std::string strlower = str;
+    std::for_each(strlower.begin(), strlower.end(), [](char& c) {
+        c = ::tolower(c);
+    });
+
+    return strlower;
+}
+
+bool CommanderBase::TryParseInt(std::string str, int& num)
+{
+    char* end;
+    num = strtol(str.c_str(), &end, 10);
+    return *end == '\0';
+}
+
+Command ConsoleCommander::GetCommand(std::vector<std::string>& args)
+{
+    std::cout << "Please enter command : ";
+
+    std::string userInput;
+    std::getline(std::cin, userInput);
+
+    const auto cmdArgs = Split(userInput, " ");
+    if (cmdArgs.size() == 0)
+        return cmdUNKNOWN;
+
+    const auto commandItr = m_commands.find(ToLower(cmdArgs[0]));
+    if (commandItr == m_commands.end())
+        return cmdUNKNOWN;
+
+    for (size_t idx = 1; idx < cmdArgs.size(); idx++)
+        args.push_back(cmdArgs[idx]);
+
+    return commandItr->second;
+}
---
+++ src/ToyRobot/Commander.h
@@ -0,0 +1,78 @@
+/*
+ *	The MIT License (MIT)
+ *	Copyright © 2021 Kushan Fernando (info@kushan.me)
+ *
+ *	Permission is hereby granted, free of charge, to any person obtaining
+ *  a copy of this software and associated documentation files (the “Software”),
+ *  to deal in the Software without restriction, including without limitation
+ *  the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ *  sell copies of the Software, and to permit persons to whom the Software is furnished
+ *  to do so, subject to the following conditions:
+ *
+ *	The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ *	THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ *  INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ *  PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ *  FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ *  ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#pragma once
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include "ToyRobot.h"
+#include "Commands.h"
+#include <iostream>
+#include "Logger.h"
+
+class CommanderBase
+{
+public:
+    CommanderBase(ToyRobot& robot, LoggerBase& logger);
+
+    void Launch();
+
+protected:
+    Command virtual GetCommand(std::vector<std::string>& args) = 0;
+
+    void Place(std::vector<std::string>& args);
+    void Move();
+    void Report();
+    std::vector<std::string> Split(const std::string& str, const std::string& delimiter);
+    std::string ToLower(std::string str);
+    bool TryParseInt(std::string str, int& num);
+
+    const std::map<std::string, Command> m_commands = {
+        { "place", cmdPLACE },
+        { "move", cmdMOVE },
+        { "left", cmdTURN_LEFT },
+        { "right", cmdTURN_RIGHT},
+        { "report", cmdREPORT},
+        { "exit", cmdEXIT }
+    };
+
+    const std::map<std::string, FacingDirection> m_facingDirections = {
+        { "north", fdNORTH },
+        { "south", fdSOUTH },
+        { "east", fdEAST },
+        { "west", fdWEST },
+    };
+
+    std::map<FacingDirection, std::string> m_facingDirectionStrings;
+
+    ToyRobot& m_robot;
+    LoggerBase& m_logger;
+};
+
+class ConsoleCommander : public CommanderBase
+{
+public:
+    ConsoleCommander(ToyRobot& robot, LoggerBase& logger)
+        : CommanderBase(robot, logger)
+    {}
+
+    Command GetCommand(std::vector<std::string>& args) override;
+};
